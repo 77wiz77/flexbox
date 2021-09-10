8df91c6fd36bc7170329b642f85a61cf30f52ad3
77wiz77 <47847427+77wiz77@users.noreply.github.com>
2021-09-10 15:01:34 +0300

    ready

---
+++ assets/js/archor.js
@@ -0,0 +1,20 @@
+"use strict"
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+  link.addEventListener('click', function(e) {
+      e.preventDefault(); 
+
+      let href = this.getAttribute('href').substring(1); 
+
+      const scrollTarget = document.getElementById(href);
+
+      const marginTop = 40; 
+      const achorPosition = scrollTarget.getBoundingClientRect().top; 
+      const finalTopPosition = achorPosition - marginTop;
+
+      window.scrollBy({
+          top: finalTopPosition,
+          behavior: 'smooth'
+      });
+  });
+});
